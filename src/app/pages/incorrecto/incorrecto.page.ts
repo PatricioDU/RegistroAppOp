f75ebc482b8bedc2e6a6ec92e9432831c1e0c916
Mclovin999 <136486890+Mclovin999@users.noreply.github.com>
2024-09-30 09:56:42 -0300

    2222

--- src/app/pages/incorrecto/incorrecto.page.ts
+++ src/app/pages/incorrecto/incorrecto.page.ts
@@ -4,12 +4,12 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-incorrecto',
   templateUrl: './incorrecto.page.html',
-  styleUrls: ['./incorrecto.page.scss'] // Verifica que esta ruta sea correcta
+  styleUrls: ['./incorrecto.page.scss'],
 })
 export class IncorrectoPage {
   constructor(private router: Router) { }
 
   goToLogin() {
-    this.router.navigate(['/login.page.html']);
+    this.router.navigate(['/login']);
   }
 }
