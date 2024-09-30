97d06b3b4e4ce4d26eeaab0f2e2a7fe425d6e204
Mclovin999 <136486890+Mclovin999@users.noreply.github.com>
2024-09-30 09:40:10 -0300

    1111

--- src/app/pages/incorrecto/incorrecto.page.ts
+++ src/app/pages/incorrecto/incorrecto.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-incorrecto',
   templateUrl: './incorrecto.page.html',
-  styleUrls: ['./incorrecto.page.scss'],
+  styleUrls: ['./incorrecto.page.scss'] // Verifica que esta ruta sea correcta
 })
-export class IncorrectoPage implements OnInit {
+export class IncorrectoPage {
+  constructor(private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  goToLogin() {
+    this.router.navigate(['/login.page.html']);
   }
-
 }
