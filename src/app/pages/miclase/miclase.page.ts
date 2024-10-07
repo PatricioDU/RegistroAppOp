c39ce1736183c74268a2e9b30244352c6ae89c31
TuTooH <135929858+TuTooH@users.noreply.github.com>
2024-10-07 14:21:30 -0300

    experimental

--- src/app/pages/miclase/miclase.page.ts
+++ src/app/pages/miclase/miclase.page.ts
@@ -1,10 +1,7 @@
-
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Route, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import jsQR, { QRCode } from 'jsqr';
-import { Asistencia } from 'src/app/model/asistencia';
-import { NivelEducacional } from 'src/app/model/nivel-educacional';
-import { Usuario } from 'src/app/model/usuario';
 
 
 @Component({
@@ -14,110 +11,158 @@ import { Usuario } from 'src/app/model/usuario';
 })
 export class miclasePage implements OnInit {
 
-  @ViewChild('video') private video!: ElementRef;
-  @ViewChild('canvas') private canvas!: ElementRef;
+  @ViewChild('fileinput', {static: false}) private fileinput!: ElementRef;
+  @ViewChild('video', {static: false}) private video!: ElementRef;
+  @ViewChild('canvas', {static: false}) private canvas!: ElementRef;
 
-  public asistencia: Asistencia | undefined = undefined;
-  public usuario: Usuario;
+
   public escaneando = false;
-  public datosQR: string = '';
-  public listaNivelesEducacionales = NivelEducacional.getNivelesEducacionales();
+  public datosQR : string = '';
+  public loading : HTMLIonLoadingElement | null = null;
+
+
+  public bloqueInicio: number = 0;
+  public bloqueTermino: number =0;
+  public dia: string = '';
+  public horaFin: string = '';
+  public horaInicio: string = '';
+  public idAsignatura: string = '';
+  public nombreAsignatura: string = '';
+  public nombreProfesor: string = '';
+  public seccion: string = '';
+  public sede: string = '';
+  usuario: any;
+
 
 
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private router: Router
-
-  ){
-    this.usuario = new Usuario();
-    this.usuario.recibirUsuario(this.activatedRoute, this.router);
-  }
+    private loadingController: LoadingController,
+    private router : Router) { }
 
   ngOnInit() {
-    this.comenzarEscaneoQR();
   }
 
-  public simularCapturaQR() {
-    this.mostrarDatosQROrdenados(`
-      {
-        "sede": "Alonso Ovalle",
-        "idAsignatura": "PGY4121",
-        "seccion": "001D",
-        "nombreAsignatura": "Aplicaciones Móviles",
-        "nombreProfesor": "Cristián Gómez Vega",
-        "dia": "2022-08-09",
-        "bloqueInicio": 7,
-        "bloqueTermino": 9,
-        "horaInicio": "13:00",
-        "horaFin": "15:15"
-      }
-    `);
-  }
-
-  public async comenzarEscaneoQR() {
-    const mediaProvider: MediaProvider = await navigator.mediaDevices.getUserMedia({
-      video: {facingMode: 'environment'}
-    });
-    this.video.nativeElement.srcObject = mediaProvider;
-    this.video.nativeElement.setAttribute('playsinline', 'true');
-    this.video.nativeElement.play();
-    this.escaneando = true;
-    requestAnimationFrame(this.verificarVideo.bind(this));
-  }
-
-  async verificarVideo() {
-    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
-      if (this.obtenerDatosQR() || !this.escaneando) return;
-      requestAnimationFrame(this.verificarVideo.bind(this));
-    } else {
-      requestAnimationFrame(this.verificarVideo.bind(this));
+  public obtenerDatosQr(source?: CanvasImageSource): boolean {
+    let w = 0;
+    let h = 0;
+    if (!source) {
+      this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
+      this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
     }
-  }
+    w = this.canvas.nativeElement.width;
+    h = this.canvas.nativeElement.height;
 
-
-  public obtenerDatosQR(): boolean {
-    const w: number = this.video.nativeElement.videoWidth;
-    const h: number = this.video.nativeElement.videoHeight;
-    this.canvas.nativeElement.width = w;
-    this.canvas.nativeElement.height = h;
-    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d', { willReadFrequently: true });
-    context.drawImage(this.video.nativeElement, 0, 0, w, h);
+    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(source ? source : this.video.nativeElement, 0, 0, w, h);
     const img: ImageData = context.getImageData(0, 0, w, h);
-    let qrCode: QRCode | null = jsQR(img.data, w, h, { inversionAttempts: 'dontInvert' });
+    const qrCode = jsQR(img.data, img.width, img.height, { inversionAttempts: 'dontInvert' });
     if (qrCode) {
-      if (qrCode.data !== '') {
-        this.escaneando = false;
-        this.mostrarDatosQROrdenados(qrCode.data);
-        return true;
-      }
+      this.escaneando = false;
+      this.datosQR = qrCode.data;
+      this.mostrarDatosQROrdenados(this.datosQR);
+      this.irAMiClase();
     }
-    return false;
+
+    return this.datosQR !== '';
+  }
+
+  public mostrarDatosQROrdenados(datosQR: string): void {
+    const objetoDatosQR = JSON.parse(datosQR);
+    this.bloqueInicio = objetoDatosQR.bloqueInicio;
+    this.bloqueTermino = objetoDatosQR.bloqueTermino;
+    this.dia = objetoDatosQR.dia;
+    this.horaFin = objetoDatosQR.horaFin;
+    this.horaInicio = objetoDatosQR.horaInicio;
+    this.idAsignatura = objetoDatosQR.idAsignatura;
+    this.nombreAsignatura = objetoDatosQR.nombreAsignatura;
+    this.nombreProfesor = objetoDatosQR.nombreProfesor;
+    this.seccion = objetoDatosQR.seccion;
+    this.sede = objetoDatosQR.sede;
+  }
+
+  public irAMiClase(): void {
+    this.router.navigate(['/tabs/mi-clase'], {
+      queryParams: {
+        bloqueInicio: this.bloqueInicio,
+        bloqueTermino: this.bloqueTermino,
+        dia: this.dia,
+        horaFin: this.horaFin,
+        horaInicio: this.horaInicio,
+        idAsignatura: this.idAsignatura,
+        nombreAsignatura: this.nombreAsignatura,
+        nombreProfesor: this.nombreProfesor,
+        seccion: this.seccion,
+        sede: this.sede
+      }
+    });
+  }
+
+  public verificarArchivoConQr(event: Event): void {
+    const input = event.target as HTMLInputElement;  // Asegura que el target es un input
+    if (input && input.files && input.files.length > 0) {
+      const file = input.files[0];  // Accede al primer archivo
+      const img = new Image();
+      img.onload = () => {
+        this.obtenerDatosQr(img);
+      };
+      img.src = URL.createObjectURL(file);  // Carga la imagen
+    }
+  }
+
+  public async comenzarEscaneoQR(){
+    try {
+      const mediaProvider: MediaStream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: 'environment' }
+      });
+      this.video.nativeElement.srcObject = mediaProvider;
+      this.video.nativeElement.setAttribute('playsinline', 'true');
+      this.loading = await this.loadingController.create({
+        message: 'Iniciando escaneo...'
+      });
+      await this.loading.present();
+      this.video.nativeElement.play();
+      this.escaneando = true; // Actualiza el estado a escaneando
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    } catch (error) {
+      console.error('Error al acceder a la cámara:', error);
+      // Manejar el error adecuadamente
+    }
+  }
+  async verificarVideo(){
+    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA){
+      if (this.loading){
+        await this.loading.dismiss();
+        this.loading = null;
+        this.escaneando =true;
+      }
+      if(this.obtenerDatosQr()){
+        console.log('datos obtenidos');
+      } else {
+        if(this.escaneando){
+          console.log('escaneando...');
+          requestAnimationFrame(this.verificarVideo.bind(this));
+        }
+      }
+    } else {console.log('video aún no tiene datos');
+       requestAnimationFrame(this.verificarVideo.bind(this));
+    }
   }
   
-  public mostrarDatosQROrdenados(datosQR: string): void {
-    this.datosQR = datosQR;
-    this.usuario.asistencia = JSON.parse(datosQR);
-    this.usuario.navegarEnviandoUsuario(this.router, 'miclase');
-  }
-
   public detenerEscaneoQR(): void {
-    this.escaneando = false;
+    this.escaneando = false; // Actualiza el estado a no escaneando
+    // Detén el video y libera la cámara
+    const stream = this.video.nativeElement.srcObject;
+    if (stream) {
+      stream.getTracks().forEach((track: MediaStreamTrack) => track.stop());
+    }
+    this.video.nativeElement.srcObject = null;
   }
-
+ 
   navegar(pagina: string) {
-    this.usuario.navegarEnviandoUsuario(this.router, pagina);
+    this.usuario.navegarEnviandousuario(this.router, pagina);
   }
-
-
-
-
-
-
-
-
-
-
 
 
 }
+
